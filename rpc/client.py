b1b98092f3804a1f62b7e2ef0a7369548afbb1be
xmwu_mac <243399634@qq.com>
2020-07-14 23:01:22 +0800

    add rpc

---
+++ rpc/client.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : client.py
+@Time    : 2020-07-03 16:31
+@Author  : wuxiaomin
+@Email   : xmwu@mail.ustc.edu.cn
+@Description:
+"""
+
+import zerorpc
+from server import Entity
+import msgpack
+import pickle
+
+
+
+if __name__ == '__main__':
+    c = zerorpc.Client()
+    c.connect("tcp://127.0.0.1:4242")
+    print(c.hello("RPC"))
+    e = Entity("xiaomin", 30)
+    print(c.hello_entity(pickle.dumps(e)))
