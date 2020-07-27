c14ca6af27ca8e77da465df4f5bfd1f8fc8ab407
xmwu_mac <243399634@qq.com>
2020-07-27 15:24:21 +0800

    scrapy

---
+++ pytorch/torch_dqn.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import gym
+
+
+BATCH_SIZE = 32
+LR = 0.01
+EPSILON = 0.9
+GAMMA = 0.9
+TARGET_REPLACE_ITER = 100
+MEMORY_SIZE = 200
+
+env = gym.make("CartPole-v0")
+env = env.unwrapped
+
+N_ACTIONS = env.action_space.n
+N_STATES = env.observation_space.shape[0]
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(N_STATES, 10)
+        self.fc1.weight.data.normal_(0, 0.1)
+        self.out = nn.Linear(10, N_ACTIONS)
+        self.out.weight.data.normal_(0, 0.1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.relu(x)
+        action = self.out(x)
+        return action
+
+
+class DQN(object):
+
+    def __init__(self):
+        self.eval_net , self.target_net = Net(), Net()
+        self.learn_step_counter = 0
+        self.memory_counter = 0
+        self.memory = np.zeros((MEMORY_SIZE, N_STATES*2 + 2))
+        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)
+        self.loss_func = nn.MSELoss()
+
+    def choose_action(self, x):
+        x = torch.unsqueeze(torch.FloatTensor(x), 0)
+        if np.random.uniform() < EPSILON:
+            action = self.eval_net.forward(x)
+            action = torch.max(action, 1)[1].numpy()[0]
+        else:
+            action = np.random.randint(0, N_ACTIONS)
+        return action
+
+    def store_eps(self, s, a, r, s_):
+        eps = np.hstack((s, a, r, s_))
+        index = self.memory_counter % MEMORY_SIZE
+        self.memory[index, :] = eps
+        self.memory_counter += 1
+
+    def learn(self):
+        if self.learn_step_counter % TARGET_REPLACE_ITER == 0:
+            self.target_net.load_state_dict(self.eval_net.state_dict())
+        self.learn_step_counter += 1
+        sample_idxs = np.random.choice(MEMORY_SIZE, BATCH_SIZE)
+        b_memory = self.memory[sample_idxs, :]
+        b_s = torch.FloatTensor(b_memory[:, :N_STATES])
+        b_a = torch.LongTensor(b_memory[:, N_STATES:N_STATES+1].astype(int))
+        b_r = torch.FloatTensor(b_memory[:, N_STATES+1:N_STATES+2])
+        b_s_ = torch.FloatTensor(b_memory[:, -N_STATES:])
+
+        q_eval = self.eval_net(b_s).gather(1, b_a)
+        q_next = self.target_net(b_s_).detach()
+        q_target = b_r + GAMMA * q_next.max(1)[0].unsqueeze(1)
+        loss = self.loss_func(q_eval, q_target)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+
+if __name__ == '__main__':
+    dqn = DQN()
+
+    for eps_id in range(10):
+        print("========= eps:{}".format(eps_id))
+        s = env.reset()
+        while True:
+            env.render()
+            a = dqn.choose_action(s)
+
+            s_, r, done, info = env.step(a)
+
+            x, x_dot, theta, theta_dot = s_
+            r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
+            r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
+            r = r1 + r2
+
+            dqn.store_eps(s, a, r, s_)
+
+            if dqn.memory_counter > MEMORY_SIZE:
+                dqn.learn()
+
+            if done:
+                break
+            s = s_
+    env.close()
+
+
+
+
---
+++ scrapy/image_scrapy.py
@@ -0,0 +1,71 @@
+import os
+from urllib.parse import urljoin, urlsplit, urlunsplit
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class ImageScrapy(object):
+
+    def __init__(self, full_url, save_root="./", filters=None):
+        self.full_url = full_url
+        self.save_root = save_root
+        parts = urlsplit(full_url)
+        self.web_root = urlunsplit((parts.scheme, parts.netloc, '', '', ''))
+        self.html = None
+        self.filters = filters
+
+    def get_html(self):
+        try:
+            read = requests.get(self.full_url, timeout=2)
+            read.raise_for_status()
+            read.encoding = read.apparent_encoding
+            self.html = read.text
+        except Exception as e:
+            print(e)
+
+    def get_html_title(self, soup):
+        # return soup.find('title').text.strip()
+        return soup.find('title').text.split(' ')[0].strip()
+
+    def get_abs_url(self, path):
+        return urljoin(self.web_root, path)
+
+    def download_img(self):
+        self.get_html()
+        soup = BeautifulSoup(self.html, "html.parser")
+        # all_images = soup.find_all('img', attrs={'style': 'cursor: pointer '})
+        all_images = soup.find_all('img')
+        for filter_func in self.filters:
+            all_images = filter(filter_func, all_images)
+        if all_images:
+            root_path = self.get_html_title(soup)
+            try:
+                if not os.path.exists(root_path):
+                    os.mkdir(root_path)
+            except:
+                print("can not create root directory")
+        for idx, img in enumerate(all_images):
+            # img_url = img['src']
+            img_url = img['ess-data']
+            # print(img_url)
+            # img_url = img_url.replace('thumb_', '')
+            img_url = self.get_abs_url(img_url)
+            print(img_url)
+            img_ext = img_url.split('.')[-1]
+            path = os.path.join(root_path, "{}.{}".format(idx, img_ext))
+            print(path)
+            try:
+                if not os.path.exists(path):
+                    read = requests.get(img_url)
+                    with open(path, mode='wb') as f:
+                        f.write(read.content)
+            except:
+                print("cannot save image")
+
+
+if __name__ == '__main__':
+    # url = "https://forum.xitek.com/thread-1890437-1-1-1.html"
+    url = "https://cl.ht52.xyz/htm_data/2007/7/4022171.html"
+    scrapy = ImageScrapy(url, filters=[lambda x: x.get("ess-data")])
+    scrapy.download_img()
