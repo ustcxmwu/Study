8710bc5d3358b21761015bdcc78d76701b706c97
ustcxmwu <ustcxmwu@gmail.com>
2022-07-06 10:22:21 +0800

    type

---
+++ python_language/meta/type_client.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022.07.04 14:41
+# @Author  : Xiaomin Wu
+# @Email   : ustcxmwu@gmai.com
+# @Desc    :
+
+
+if __name__ == '__main__':
+    a = type("Student", (), {"name": "xxx"})
+    print(a)
+    print(a.name)
