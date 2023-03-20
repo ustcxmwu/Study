ff0c63c056b001db4e89d8c1de0c2e70954cdd3b
Xiaomin Wu <243399634@qq.com>
2023-03-20 09:18:34 +0800

    adjust img download path

--- application/image_scrapy/image_scrapy.py
+++ application/image_scrapy/image_scrapy.py
@@ -4,6 +4,7 @@ from urllib.parse import urljoin, urlsplit, urlunsplit
 
 import bs4
 import gradio as gr
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -33,9 +34,8 @@ class ImageScrapy(object):
         except Exception as e:
             print(e)
 
-    def get_html_title(self, soup):
-        # return soup.find('title').text.strip()
-        return soup.find('title').text.split(' ')[0].strip()
+    def get_html_title(self, soup, img_num: int):
+        return re.split(r"[\b\W\b]+", soup.find('title').text)[0] + "[" + str(img_num) + "P]"
 
     def get_abs_url(self, path):
         return urljoin(self.web_root, path)
@@ -45,18 +45,15 @@ class ImageScrapy(object):
         soup = BeautifulSoup(html_page, "lxml")
         all_images = soup.find_all(selector)
         if all_images:
-            root_path = Path(self.get_html_title(soup))
+            root_path = Path(self.get_html_title(soup, len(all_images)))
             root_path.mkdir(exist_ok=True)
             for idx, img in enumerate(all_images):
                 img_url = img.get(img_link_attr)
                 if img_url is None:
                     print(f"image tag: {img} can not parse image link.")
                     continue
-                img_url = self.get_abs_url(img_url)
-                print(img_url)
-                img_ext = img_url.split('.')[-1]
-                path = root_path / "{}.{}".format(idx, img_ext)
-                print(path)
+                path = root_path / f"{idx}{Path(img_url).suffix}"
+                print(img_url, path)
                 with self.sess.get(img_url, headers=headers, stream=True) as r:
                     with open(path, "wb") as f:
                         for chunk in r.iter_content(chunk_size=1024):
@@ -69,9 +66,13 @@ class ImageScrapy(object):
 def xyz_selector(tag: bs4.Tag) -> bool:
     return tag.parent.get("id", "") == "conttpc" and tag.name == "img"
 
+def xp1024_selector(tag: bs4.Tag) -> bool:
+    return tag.parent.get("id", "") == "read_tpc" and tag.name == "img"
+
 
 selectors = {
-    "xyz": (xyz_selector, "ess-data")
+    "xyz": (xyz_selector, "ess-data"),
+    "xp1024": (xp1024_selector, "src"),
 }
 
 
@@ -80,20 +81,24 @@ def parse_images(url: str, selector: str) -> int:
     return scrapy.download_img(*selectors[selector])
 
 
+demo = gr.Interface(
+    fn=parse_images,
+    # inputs="text",
+    inputs=[
+        gr.Textbox(
+            label="Image URL",
+            lines=1,
+            value="The quick brown fox jumped over the lazy dogs.",
+        ),
+        gr.Dropdown(
+            ["xyz", "xp1024"],
+            label="Selector",
+            info="Will add more selectors later!"
+        ),
+    ],
+    outputs="text")
+
 if __name__ == '__main__':
     # https://cl.3572x.xyz/htm_mob/2303/7/5601509.html
-    demo = gr.Interface(
-        fn=parse_images,
-        # inputs="text",
-        inputs=[
-            gr.Textbox(
-                label="Image URL",
-                lines=1,
-                value="The quick brown fox jumped over the lazy dogs.",
-            ),
-            gr.Dropdown(
-                ["xyz"], label="Selector", info="Will add more selectors later!"
-            ),
-        ],
-        outputs="text")
-    demo.launch()
+    # https://yj2303z.xyz/pw/html_data/15/2303/6536994.html
+    demo.launch(server_port=9999)
