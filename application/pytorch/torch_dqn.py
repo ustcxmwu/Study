bf683d865524c2621286eaee258441f1582ad6d7
Xiaomin Wu <243399634@qq.com>
2023-12-15 10:16:17 +0800

    rl dqn success

--- application/pytorch/torch_dqn.py
+++ application/pytorch/torch_dqn.py
@@ -12,7 +12,7 @@ GAMMA = 0.9
 TARGET_REPLACE_ITER = 100
 MEMORY_SIZE = 200
 
-env = gym.make("CartPole-v0")
+env = gym.make("CartPole-v1", render_mode="human")
 env = env.unwrapped
 
 N_ACTIONS = env.action_space.n
@@ -20,7 +20,6 @@ N_STATES = env.observation_space.shape[0]
 
 
 class Net(nn.Module):
-
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(N_STATES, 10)
@@ -36,20 +35,21 @@ class Net(nn.Module):
 
 
 class DQN(object):
-
     def __init__(self):
-        self.eval_net , self.target_net = Net(), Net()
+        self.eval_net, self.target_net = Net(), Net()
         self.learn_step_counter = 0
         self.memory_counter = 0
-        self.memory = np.zeros((MEMORY_SIZE, N_STATES*2 + 2))
+        self.memory = np.zeros((MEMORY_SIZE, N_STATES * 2 + 2))
         self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)
         self.loss_func = nn.MSELoss()
 
     def choose_action(self, x):
         x = torch.unsqueeze(torch.FloatTensor(x), 0)
+        # x = torch.tensor(x[0])
         if np.random.uniform() < EPSILON:
             action = self.eval_net.forward(x)
-            action = torch.max(action, 1)[1].numpy()[0]
+            # action = torch.max(action, 1)[1].numpy()[0]
+            action = int(torch.max(action).detach().numpy())
         else:
             action = np.random.randint(0, N_ACTIONS)
         return action
@@ -67,8 +67,8 @@ class DQN(object):
         sample_idxs = np.random.choice(MEMORY_SIZE, BATCH_SIZE)
         b_memory = self.memory[sample_idxs, :]
         b_s = torch.FloatTensor(b_memory[:, :N_STATES])
-        b_a = torch.LongTensor(b_memory[:, N_STATES:N_STATES+1].astype(int))
-        b_r = torch.FloatTensor(b_memory[:, N_STATES+1:N_STATES+2])
+        b_a = torch.LongTensor(b_memory[:, N_STATES : N_STATES + 1].astype(int))
+        b_r = torch.FloatTensor(b_memory[:, N_STATES + 1 : N_STATES + 2])
         b_s_ = torch.FloatTensor(b_memory[:, -N_STATES:])
 
         q_eval = self.eval_net(b_s).gather(1, b_a)
@@ -81,21 +81,23 @@ class DQN(object):
         self.optimizer.step()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dqn = DQN()
 
     for eps_id in range(10):
         print("========= eps:{}".format(eps_id))
-        s = env.reset()
+        s, reset_info = env.reset()
         while True:
             env.render()
             a = dqn.choose_action(s)
 
-            s_, r, done, info = env.step(a)
+            s_, r, done, time_up, info = env.step(a)
 
             x, x_dot, theta, theta_dot = s_
             r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
-            r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
+            r2 = (
+                env.theta_threshold_radians - abs(theta)
+            ) / env.theta_threshold_radians - 0.5
             r = r1 + r2
 
             dqn.store_eps(s, a, r, s_)
@@ -107,7 +109,3 @@ if __name__ == '__main__':
                 break
             s = s_
     env.close()
-
-
-
-
